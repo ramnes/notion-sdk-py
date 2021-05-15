41f8be181ecdf7f46b79f504c7f0eb735cd32797
Jack Eadie <jeadi@amazon.com>
2021-05-15 15:21:30 +1000

    Page object deserialisation, with just database properties, not full fledged properties

--- notion_client/api_objects.py
+++ notion_client/api_objects.py
@@ -9,6 +9,10 @@ class RichTextType(Enum):
     mention = "mention"
     equation = "equation"
 
+class ParentType(Enum):
+    page = "page_id"
+    database = "database_id"
+    workspace = "workspace"
 
 class BasicColor(Enum):
     default = "default"
@@ -118,6 +122,7 @@ class Annotations:
             color=Color(d["color"])
         )
 
+
 @dataclass
 class RichText:
     plain_text: str
@@ -131,7 +136,7 @@ class RichText:
             plain_text=d.get('plain_text'),
             href=d.get('href'),
             annotations=Annotations.from_dict(d.get("annotations")),
-            type = RichTextType(d["type"])
+            type=RichTextType(d["type"])
         )
 
 
@@ -158,6 +163,7 @@ class DatabaseProperty:
             id=d["id"],
             type=DatabasePropertyType(d["type"])
         )
+
 
 @dataclass
 class FormulaDatabaseProperty(DatabaseProperty):
@@ -259,7 +265,7 @@ class LastEditedByDatabaseProperty(DatabaseProperty):
     pass
 
 
-def property_from_dict(d: Dict[str, object]) -> DatabaseProperty:
+def database_property_from_dict(d: Dict[str, object]) -> DatabaseProperty:
     property_type = d['type']
     property_type_to_class = {
         "title": TitleDatabaseProperty,
@@ -285,23 +291,87 @@ def property_from_dict(d: Dict[str, object]) -> DatabaseProperty:
     return property_type_to_class[property_type].from_dict(d)
 
 
+def page_property_from_dict(d: Dict[str, object]) -> DatabaseProperty:
+    property_type = d['type']
+    property_type_to_class = {
+        "title": TitleDatabaseProperty,
+        "rich_text": RichTextDatabaseProperty,
+        "number": NumberDatabaseProperty,
+        "select": SelectDatabaseProperty,
+        "multi_select": MultiselectDatabaseProperty,
+        "date": DateDatabaseProperty,
+        "people": PeopleDatabaseProperty,
+        "file": FileDatabaseProperty,
+        "checkbox": CheckboxDatabaseProperty,
+        "url": UrlDatabaseProperty,
+        "email": EmailDatabaseProperty,
+        "phone_number": PhoneNumberDatabaseProperty,
+        "formula": FormulaDatabaseProperty,
+        "relation": RelationDatabaseProperty,
+        "rollup": RollupDatabaseProperty,
+        "created_time": CreatedTimeDatabaseProperty,
+        "created_by": CreatedByDatabaseProperty,
+        "last_edited_time": LastEditedTimeDatabaseProperty,
+        "last_edited_by": LastEditedByDatabaseProperty
+    }
+    return property_type_to_class[property_type].from_dict(d)
+
 @dataclass
-class DatabaseObject:
+class APIObject:
     id: str
+    object: str
     created_time: datetime
     last_edited_time: datetime
+
+
+@dataclass
+class DatabaseObject(APIObject):
     title: List[RichText]
     properties: Dict[str, DatabaseProperty]
-    object: str = "database"
+
     @classmethod
     def from_dict(cls, d: Dict[str, object]):
         return DatabaseObject(
             id=d['id'],
+            object="database",
             created_time=datetime.strptime(d['created_time'], "%Y-%m-%dT%H:%M:%S.%fZ"),
             last_edited_time=datetime.strptime(d['last_edited_time'], "%Y-%m-%dT%H:%M:%S.%fZ"),
             title=RichText.from_dict(d['title'][0]),
             properties=dict([
-                (k, property_from_dict(v)) for (k, v) in d["properties"].items()
+                (k, database_property_from_dict(v)) for (k, v) in d["properties"].items()
             ])
         )
 
+@dataclass
+class PageParent:
+    type: ParentType
+    id: str
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, object]):
+        type = ParentType(d['type'])
+        return PageParent(
+            id=d.get("database_id" if type == ParentType.database else "page_id"),
+            type=type,
+        )
+
+@dataclass
+class PageObject(APIObject):
+    parent: PageParent
+    archived: bool
+    properties: Dict[str, DatabaseProperty]
+
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, object]):
+        return PageObject(
+            id=d['id'],
+            object="page",
+            archived=d['archived'],
+            created_time=datetime.strptime(d['created_time'], "%Y-%m-%dT%H:%M:%S.%fZ"),
+            last_edited_time=datetime.strptime(d['last_edited_time'], "%Y-%m-%dT%H:%M:%S.%fZ"),
+            parent=PageParent.from_dict(d['parent']),
+            properties=dict([
+                (k, page_property_from_dict(v)) for (k, v) in d["properties"].items()
+            ])
+        )
