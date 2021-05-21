bef1ded23290baca4e59a9bd3cc4384facafecdd
aahnik <daw@aahnik.dev>
2021-05-21 11:18:41 +0530

    add the docs/ directory with all its glory
    - generate.py is a virtual file generator for mkdocs
    - quick_start.md for easy and precise starting guide
    - SUMMARY.md for the site navbar

---
+++ docs/SUMMARY.md
@@ -0,0 +1,12 @@
+<!-- markdownlint-disable -->
+* Home
+    * [Overview](index.md)
+    * [Quick Start](quick_start.md)
+    * [License](license.md)
+* Reference
+    * [Client](reference/client.md)
+    * [API Endpoints](reference/api_endpoints.md)
+    * [Errors](reference/errors.md)
+* Development
+    * [Coverage Report](coverage.md)
+    * [Contributing Guidelines](contributing/contributing.md)
---
+++ docs/generate.py
@@ -0,0 +1,22 @@
+"""Generate virtual files for mkdocs."""
+
+import mkdocs_gen_files
+
+
+def docs_stub(module_name):
+    return f"::: notion_client.{module_name}\
+        \n\trendering:\n\t\tshow_root_heading: true\n\t\tshow_source: false"
+
+
+virtual_files = {
+    "index.md": "--8<-- 'README.md'",
+    "license.md": "```text\n--8<-- 'LICENSE'\n```",
+    "reference/api_endpoints.md": docs_stub("api_endpoints"),
+    "reference/client.md": docs_stub("client"),
+    "reference/errors.md": docs_stub("errors"),
+    "contributing/contributing.md": "--8<-- '.github/CONTRIBUTING.md'",
+}
+
+for file_name, content in virtual_files.items():
+    with mkdocs_gen_files.open(file_name, "w") as file:
+        print(content, file=file)
