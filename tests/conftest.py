22c8cdc31c13c54bdbed17d0d0a01b2623c3e562
ramnes <contact@ramnes.eu>
2021-06-09 22:34:45 +0200

    Test {AsyncClient,Client}.request()

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,24 @@ import os
 
 import pytest
 
+from notion_client import AsyncClient, Client
+
+
+def pytest_collection_modifyitems(items):
+    for item in items:
+        item.add_marker("asyncio")
+
 
 @pytest.fixture(scope="session")
 def token():
     return os.environ["NOTION_TOKEN"]
+
+
+@pytest.fixture
+def client(token):
+    return Client({"auth": token})
+
+
+@pytest.fixture
+def async_client(token):
+    return AsyncClient({"auth": token})
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,9 +1,27 @@
-from notion_client import AsyncClient, Client
+import pytest
+
+from notion_client import APIResponseError, AsyncClient, Client
 
 
-def test_initialize_client(token):
-    client = Client({"auth": token})
-    assert client
+def test_client_init(client):
+    assert isinstance(client, Client)
 
-    async_client = AsyncClient({"auth": token})
-    assert async_client
+
+def test_async_client_init(async_client):
+    assert isinstance(async_client, AsyncClient)
+
+
+def test_client_request(client):
+    with pytest.raises(APIResponseError):
+        client.request("/invalid", "GET")
+
+    response = client.request("/users", "GET")
+    assert response["results"]
+
+
+async def test_async_client_request(async_client):
+    with pytest.raises(APIResponseError):
+        await async_client.request("/invalid", "GET")
+
+    response = await async_client.request("/users", "GET")
+    assert response["results"]
