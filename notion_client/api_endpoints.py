688740fb62d9eaed24ad86ca04e5c3e1ccb9ff34
ramnes <contact@ramnes.eu>
2021-10-18 11:12:22 +0200

    Update Client.blocks.delete() docs

--- notion_client/api_endpoints.py
+++ notion_client/api_endpoints.py
@@ -83,7 +83,11 @@ class BlocksEndpoint(Endpoint):
         )
 
     def delete(self, block_id: str, **kwargs: Any) -> SyncAsync[Any]:
-        """TBA."""
+        """Set a Block object, including page blocks, to `archived: true`.
+
+        **Note**: in the Notion UI application, this moves the block to the "Trash" where
+        it can still be accessed and restored.
+        """
         return self.parent.request(
             path=f"blocks/{block_id}",
             method="DELETE",
