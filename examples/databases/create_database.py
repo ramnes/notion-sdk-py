e15e7725152c329f61fbdbec4760c5d6eecb4e30
al1p <aprasquier@odm-tech.com>
2021-10-29 19:09:17 +0300

    Add an example of database creation (#89)

---
+++ examples/databases/create_database.py
@@ -0,0 +1,91 @@
+import os
+
+from notion_client import Client
+from notion_client.helpers import get_id
+
+NOTION_TOKEN = os.getenv("NOTION_TOKEN", "")
+
+while NOTION_TOKEN == "":
+    print("NOTION_TOKEN not found.")
+    NOTION_TOKEN = input("Enter your integration token: ").strip()
+
+# Initialize the client
+notion = Client(auth=NOTION_TOKEN)
+
+
+def manual_inputs(parent_id="", db_name="") -> tuple:
+    """
+    Get values from user input
+    """
+    if parent_id == "":
+        is_page_ok = False
+        while not is_page_ok:
+            input_text = input("\nEnter the parent page ID or URL: ").strip()
+            # Checking if the page exists
+            try:
+                if input_text[0:4] == "http":
+                    parent_id = get_id(input_text)
+                    print(f"\nThe ID of the target page is: {parent_id}")
+                else:
+                    parent_id = input_text
+                notion.pages.retrieve(parent_id)
+                is_page_ok = True
+                print("Page found")
+            except Exception as e:
+                print(e)
+                continue
+    while db_name == "":
+        db_name = input("\n\nName of the database that you want to create: ")
+
+    return (parent_id, db_name)
+
+
+def create_database(parent_id: str, db_name: str) -> dict:
+    """
+    parent_id(str): ID of the parent page
+    db_name(str): Title of the database
+    """
+    print(f"\n\nCreate database '{db_name}' in page {parent_id}...")
+    properties = {
+        "Name": {"title": {}},  # This is a required property
+        "Description": {"rich_text": {}},
+        "In stock": {"checkbox": {}},
+        "Food group": {
+            "select": {
+                "options": [
+                    {"name": "🥦 Vegetable", "color": "green"},
+                    {"name": "🍎 Fruit", "color": "red"},
+                    {"name": "💪 Protein", "color": "yellow"},
+                ]
+            }
+        },
+        "Price": {"number": {"format": "dollar"}},
+        "Last ordered": {"date": {}},
+        "Store availability": {
+            "type": "multi_select",
+            "multi_select": {
+                "options": [
+                    {"name": "Duc Loi Market", "color": "blue"},
+                    {"name": "Rainbow Grocery", "color": "gray"},
+                    {"name": "Nijiya Market", "color": "purple"},
+                    {"name": "Gus's Community Market", "color": "yellow"},
+                ]
+            },
+        },
+        "+1": {"people": {}},
+        "Photo": {"files": {}},
+    }
+    title = [{"type": "text", "text": {"content": db_name}}]
+    icon = {"type": "emoji", "emoji": "🎉"}
+    parent = {"type": "page_id", "page_id": parent_id}
+    newdb = notion.databases.create(
+        parent=parent, title=title, properties=properties, icon=icon
+    )
+    return newdb
+
+
+if __name__ == "__main__":
+
+    parent_id, db_name = manual_inputs()
+    newdb = create_database(parent_id=parent_id, db_name=db_name)
+    print(f"\n\nDatabase {db_name} created at {newdb['url']}\n")
