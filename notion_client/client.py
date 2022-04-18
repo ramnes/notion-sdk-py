c6012e538ab4df06e5687234121c1246a8cedb3a
ramnes <contact@ramnes.eu>
2022-04-18 15:33:27 +0200

    Upgrade to Notion-Version 2022-02-22
    Changelog here: https://developers.notion.com/changelog/releasing-notion-version-2022-02-22

--- notion_client/client.py
+++ notion_client/client.py
@@ -47,7 +47,7 @@ class ClientOptions:
     base_url: str = "https://api.notion.com"
     log_level: int = logging.WARNING
     logger: Optional[logging.Logger] = None
-    notion_version: str = "2021-08-16"
+    notion_version: str = "2022-02-22"
 
 
 class BaseClient:
