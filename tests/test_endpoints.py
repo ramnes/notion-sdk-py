6b6c1fa3cfdad683ddd00231267fe94218681779
mutt0-ds <davide.muttoni@gmail.com>
2022-09-29 20:10:51 +0200

    updated endpoint tests as required, with clearer namings and setup section first

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -5,21 +5,11 @@ import pytest
 TEST_PAGE_NAME = f"Test Page - {datetime.now()}"
 
 
-def compare_cleaned_ids(id1: str, id2: str) -> bool:
-    return id1.replace("-", "").strip() == id2.replace("-", "").strip()
-
-
-# PAGES
-# `test_pages_create` generates a temporary page and uses its ID
-# as input for all the other tests. Their objects (databases, blocks, comments)
-# will be created inside the page. With this method we have a
-# unique environment for each run, using the current datetime as identifier
-
-
+# SETUP
 @pytest.fixture(scope="session")
-def test_pages_create(client, testing_page) -> str:
+def session_page_id(client, page_id) -> str:
     response = client.pages.create(
-        parent={"page_id": testing_page},
+        parent={"page_id": page_id},
         properties={
             "title": [{"text": {"content": TEST_PAGE_NAME}}],
         },
@@ -34,39 +24,14 @@ def test_pages_create(client, testing_page) -> str:
     client.blocks.delete(block_id=response["id"])
 
 
-def test_pages_retrieve(client, test_pages_create, testing_page):
-    response = client.pages.retrieve(page_id=test_pages_create)
-    assert response["object"] == "page"
-    assert compare_cleaned_ids(response["parent"]["page_id"], testing_page)
-
-
-def test_pages_update(client, test_pages_create):
-    icon = {"type": "emoji", "emoji": "🛴"}
-
-    response = client.pages.update(page_id=test_pages_create, icon=icon)
-    assert response["icon"] == icon
-
-
-def test_pages_properties_retrieve(client, test_pages_create):
-    response = client.pages.properties.retrieve(
-        page_id=test_pages_create, property_id="title"
-    )
-    assert response["results"][0]["type"] == "title"
-
-
-# BLOCKS
-# `test_blocks_children_append` generates a temporary pragaraph
-# and uses its ID as input for the other tests
-
-
 @pytest.fixture(scope="session")
-def test_blocks_children_append(client, test_pages_create):
+def session_block_id(client, session_page_id) -> str:
     children = [
         {"paragraph": {"rich_text": [{"text": {"content": "I'm a paragraph."}}]}}
     ]
 
     response = client.blocks.children.append(
-        block_id=test_pages_create, children=children
+        block_id=session_page_id, children=children
     )
     assert response["type"] == "block"
     assert response["results"][0]["type"] == "paragraph"
@@ -74,23 +39,86 @@ def test_blocks_children_append(client, test_pages_create):
     return response["results"][0]["id"]
 
 
-def test_blocks_children_list(client, test_pages_create, test_blocks_children_append):
-    response = client.blocks.children.list(block_id=test_pages_create)
-    assert response["object"] == "list"
-    assert response["type"] == "block"
-    assert compare_cleaned_ids(
-        response["results"][0]["id"], test_blocks_children_append
+@pytest.fixture(scope="session")
+def session_database_id(client, session_page_id) -> str:
+    db_name = f"Test Database - {datetime.now()}"
+    properties = {
+        "Name": {"title": {}},  # required property
+    }
+    title = [{"type": "text", "text": {"content": db_name}}]
+    parent = {"type": "page_id", "page_id": session_page_id}
+    response = client.databases.create(
+        **{"parent": parent, "title": title, "properties": properties}
     )
 
+    return response["id"]
 
-def test_blocks_retrieve(client, test_blocks_children_append):
-    response = client.blocks.retrieve(block_id=test_blocks_children_append)
+
+@pytest.fixture(scope="session")
+def session_comment_id(client, session_page_id) -> str:
+    parent = {"page_id": session_page_id}
+    rich_text = [
+        {
+            "text": {
+                "content": "This is a test comment.",
+            },
+        },
+    ]
+
+    response = client.comments.create(parent=parent, rich_text=rich_text)
+
+    assert response["object"] == "comment"
+    assert compare_cleaned_ids(response["parent"]["page_id"], session_page_id)
+    assert response["rich_text"][0]["plain_text"] == rich_text[0]["text"]["content"]
+
+    return response["id"]
+
+
+def compare_cleaned_ids(id1: str, id2: str) -> bool:
+    return id1.replace("-", "").strip() == id2.replace("-", "").strip()
+
+
+# PAGES
+
+
+def test_pages_retrieve(client, session_page_id, page_id):
+    response = client.pages.retrieve(page_id=session_page_id)
+    assert response["object"] == "page"
+    assert compare_cleaned_ids(response["parent"]["page_id"], page_id)
+
+
+def test_pages_update(client, session_page_id):
+    icon = {"type": "emoji", "emoji": "🛴"}
+
+    response = client.pages.update(page_id=session_page_id, icon=icon)
+    assert response["icon"] == icon
+
+
+def test_pages_properties_retrieve(client, session_page_id):
+    response = client.pages.properties.retrieve(
+        page_id=session_page_id, property_id="title"
+    )
+    assert response["results"][0]["type"] == "title"
+
+
+# BLOCKS
+
+
+def test_blocks_children_list(client, session_page_id, session_block_id):
+    response = client.blocks.children.list(block_id=session_page_id)
+    assert response["object"] == "list"
+    assert response["type"] == "block"
+    assert compare_cleaned_ids(response["results"][0]["id"], session_block_id)
+
+
+def test_blocks_retrieve(client, session_block_id):
+    response = client.blocks.retrieve(block_id=session_block_id)
     assert response["object"] == "block"
     assert response["type"] == "paragraph"
-    assert compare_cleaned_ids(response["id"], test_blocks_children_append)
+    assert compare_cleaned_ids(response["id"], session_block_id)
 
 
-def test_blocks_update(client, test_blocks_children_append):
+def test_blocks_update(client, session_block_id):
     new_plain_text = "I'm an updated paragraph."
     new_text = {
         "rich_text": [
@@ -100,20 +128,18 @@ def test_blocks_update(client, test_blocks_children_append):
             }
         ]
     }
-    response = client.blocks.update(
-        block_id=test_blocks_children_append, paragraph=new_text
-    )
+    response = client.blocks.update(block_id=session_block_id, paragraph=new_text)
 
-    assert compare_cleaned_ids(response["id"], test_blocks_children_append)
+    assert compare_cleaned_ids(response["id"], session_block_id)
     assert response["paragraph"]["rich_text"][0]["plain_text"] == new_plain_text
 
 
-def test_blocks_delete(client, test_pages_create, test_blocks_children_append):
-    response = client.blocks.delete(block_id=test_blocks_children_append)
+def test_blocks_delete(client, session_block_id):
+    response = client.blocks.delete(block_id=session_block_id)
 
-    assert compare_cleaned_ids(response["id"], test_blocks_children_append)
+    assert compare_cleaned_ids(response["id"], session_block_id)
 
-    new_retrieve = client.blocks.retrieve(block_id=test_blocks_children_append)
+    new_retrieve = client.blocks.retrieve(block_id=session_block_id)
     assert new_retrieve["archived"]
 
 
@@ -139,7 +165,9 @@ def test_users_retrieve(client):
 
 
 # SEARCH
-def test_search(client, test_pages_create):
+
+
+def test_search(client, session_page_id):
     payload = {
         "query": TEST_PAGE_NAME,
         "sort": {
@@ -150,87 +178,46 @@ def test_search(client, test_pages_create):
 
     response = client.search(**payload)
     assert response["results"]
-    assert response["results"][0]["id"] == test_pages_create
+    assert response["results"][0]["id"] == session_page_id
 
 
 # DATABASES
-# `test_databases_create` generates a temporary database
-# and uses its ID as input for the other tests
 
 
-@pytest.fixture(scope="session")
-def test_databases_create(client, test_pages_create) -> str:
-    db_name = f"Test Database - {datetime.now()}"
-    properties = {
-        "Name": {"title": {}},  # required property
-    }
-    title = [{"type": "text", "text": {"content": db_name}}]
-    parent = {"type": "page_id", "page_id": test_pages_create}
-    response = client.databases.create(
-        **{"parent": parent, "title": title, "properties": properties}
-    )
-
-    #  the test returns the ID of the newly created database for the other tests
-    return response["id"]
-
-
-def test_databases_query(client, test_databases_create):
+def test_databases_query(client, session_database_id):
     query = {
-        "database_id": test_databases_create,
+        "database_id": session_database_id,
         "filter": {"timestamp": "created_time", "created_time": {"past_week": {}}},
     }
 
     response = client.databases.query(**query)
     assert response["object"] == "list"
-    # results should be empty, the testing DB has no rows
     assert not response["results"]
 
 
-def test_databases_retrieve(client, test_databases_create, test_pages_create):
+def test_databases_retrieve(client, session_database_id, session_page_id):
 
-    response = client.databases.retrieve(test_databases_create)
-    assert compare_cleaned_ids(response["id"], test_databases_create)
+    response = client.databases.retrieve(session_database_id)
+    assert compare_cleaned_ids(response["id"], session_database_id)
     assert response["object"] == "database"
-    assert compare_cleaned_ids(response["parent"]["page_id"], test_pages_create)
+    assert compare_cleaned_ids(response["parent"]["page_id"], session_page_id)
 
 
-def test_databases_update(client, test_databases_create):
+def test_databases_update(client, session_database_id):
     icon = {"type": "emoji", "emoji": "🔥"}
 
-    response = client.databases.update(database_id=test_databases_create, icon=icon)
+    response = client.databases.update(database_id=session_database_id, icon=icon)
     assert response["icon"] == icon
 
 
 # COMMENTS
-# `test_comments_create` generates a temporary comment
-# and uses its ID as input for the other test
 
 
-@pytest.fixture(scope="session")
-def test_comments_create(client, test_pages_create) -> str:
-    parent = {"page_id": test_pages_create}
-    rich_text = [
-        {
-            "text": {
-                "content": "This is a test comment.",
-            },
-        },
-    ]
+def test_comments_list(client, session_page_id, session_comment_id):
 
-    response = client.comments.create(parent=parent, rich_text=rich_text)
-
-    assert response["object"] == "comment"
-    assert compare_cleaned_ids(response["parent"]["page_id"], test_pages_create)
-    assert response["rich_text"][0]["plain_text"] == rich_text[0]["text"]["content"]
-
-    return response["id"]
-
-
-def test_comments_list(client, test_pages_create, test_comments_create):
-
-    response = client.comments.list(block_id=test_pages_create)
+    response = client.comments.list(block_id=session_page_id)
 
     assert response["object"] == "list"
     assert response["results"]
     assert response["results"][0]["object"] == "comment"
-    assert response["results"][0]["id"] == test_comments_create
+    assert response["results"][0]["id"] == session_comment_id
