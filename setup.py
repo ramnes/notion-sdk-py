f3a25d037d6504f9a791ebb35a144105ff4e5416
tj <tj@durable.ai>
2022-10-10 12:21:44 -0600

    Explicitly export `notion_client` as to not export `tests`.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 
 def get_description():
@@ -15,7 +15,7 @@ setup(
     description="Python client for the official Notion API",
     long_description=get_description(),
     long_description_content_type="text/markdown",
-    packages=find_packages(),
+    packages=["notion_client"],
     python_requires=">=3.7, <4",
     install_requires=[
         "httpx >= 0.15.0",
