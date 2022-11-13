ea49158c8c309d560c4ff72172c4a4333e42d5d8
mutt0-ds <davide.muttoni@gmail.com>
2022-11-13 14:46:07 +0100

    added cassettes to all endpoint tests + changed vcr configs

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 import os
+import re
+import warnings
 from datetime import datetime
 from typing import Optional
 
@@ -18,7 +20,18 @@ def vcr_config():
     return {
         "filter_headers": [("authorization", "secret_..."), ("user-agent", None)],
         "before_record_response": remove_headers,
+        "match_on": ["method", "remove_page_id_for_matches"],
     }
+
+
+@pytest.fixture(scope="module")
+def vcr(vcr):
+    def remove_page_id_for_matches(r1, r2):
+        RE_PAGE_ID = r"[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12}"
+        return re.sub(RE_PAGE_ID, r1.uri, "") == re.sub(RE_PAGE_ID, r2.uri, "")
+
+    vcr.register_matcher("remove_page_id_for_matches", remove_page_id_for_matches)
+    return vcr
 
 
 @pytest.fixture(scope="session")
@@ -29,13 +42,22 @@ def token() -> str:
     return notion_token
 
 
-@pytest.fixture(scope="session")
-def page_id() -> str:
+@pytest.fixture(scope="module")
+def page_id(vcr) -> str:
     """this is the ID of the Notion page where the tests will be executed
     the bot must have access to the page with all the capabilities enabled"""
     notion_page_id = os.environ.get("NOTION_TEST_PAGE_ID")
     if not notion_page_id:
-        raise EnvironmentError("Env variable NOTION_TEST_PAGE_ID needed for testing")
+        warnings.warn(
+            """Env variable NOTION_TEST_PAGE_ID needed for testing with new cassettes.
+            Retrieving the page_id from existing cassette...
+            """
+        )
+
+        with vcr.use_cassette("test_pages_create.yaml") as cass:
+            response = cass._serializer.deserialize(cass.data[0][1]["content"])
+            notion_page_id = response["parent"]["page_id"]
+
     return notion_page_id
 
 
--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -5,8 +5,33 @@ import pytest
 from tests.conftest import TEST_PAGE_NAME
 
 
-@pytest.fixture(scope="session")
-def session_page_id(client, page_id) -> str:
+@pytest.fixture(scope="module")
+def session_page_id(vcr) -> str:
+    with vcr.use_cassette("test_pages_create.yaml") as cass:
+        response = cass._serializer.deserialize(cass.data[0][1]["content"])
+        return response["id"]
+
+
+@pytest.fixture(scope="module")
+def session_block_id(vcr) -> str:
+    with vcr.use_cassette("test_blocks_children_create.yaml") as cass:
+        response = cass._serializer.deserialize(cass.data[0][1]["content"])
+        return response["results"][0]["id"]
+
+
+@pytest.fixture(scope="module")
+def session_database_id(vcr) -> str:
+    with vcr.use_cassette("test_databases_create.yaml") as cass:
+        response = cass._serializer.deserialize(cass.data[0][1]["content"])
+        return response["id"]
+
+
+def compare_cleaned_ids(id1: str, id2: str) -> bool:
+    return id1.replace("-", "").strip() == id2.replace("-", "").strip()
+
+
+@pytest.mark.vcr()
+def test_pages_create(client, page_id):
     response = client.pages.create(
         parent={"page_id": page_id},
         properties={
@@ -15,12 +40,35 @@ def session_page_id(client, page_id) -> str:
         children=[],
     )
 
-    yield response["id"]
-    client.blocks.delete(block_id=response["id"])
+    assert response["object"] == "page"
+    assert compare_cleaned_ids(response["parent"]["page_id"], page_id)
 
 
-@pytest.fixture(scope="session")
-def session_block_id(client, session_page_id) -> str:
+@pytest.mark.vcr()
+def test_pages_retrieve(client, session_page_id, page_id):
+    response = client.pages.retrieve(page_id=session_page_id)
+    assert response["object"] == "page"
+    assert compare_cleaned_ids(response["parent"]["page_id"], page_id)
+
+
+@pytest.mark.vcr()
+def test_pages_update(client, session_page_id):
+    icon = {"type": "emoji", "emoji": "🛴"}
+
+    response = client.pages.update(page_id=session_page_id, icon=icon)
+    assert response["icon"] == icon
+
+
+@pytest.mark.vcr()
+def test_pages_properties_retrieve(client, session_page_id):
+    response = client.pages.properties.retrieve(
+        page_id=session_page_id, property_id="title"
+    )
+    assert response["results"][0]["type"] == "title"
+
+
+@pytest.mark.vcr()
+def test_blocks_children_create(client, session_page_id) -> str:
     children = [
         {"paragraph": {"rich_text": [{"text": {"content": "I'm a paragraph."}}]}}
     ]
@@ -29,63 +77,12 @@ def session_block_id(client, session_page_id) -> str:
         block_id=session_page_id, children=children
     )
 
-    return response["results"][0]["id"]
+    assert response["object"] == "list"
+    assert len(response["results"]) > 0
+    assert response["results"][0]["id"]
 
 
-@pytest.fixture(scope="session")
-def session_database_id(client, session_page_id) -> str:
-    db_name = f"Test Database - {datetime.now()}"
-    properties = {
-        "Name": {"title": {}},  # required property
-    }
-    title = [{"type": "text", "text": {"content": db_name}}]
-    parent = {"type": "page_id", "page_id": session_page_id}
-    response = client.databases.create(
-        **{"parent": parent, "title": title, "properties": properties}
-    )
-
-    return response["id"]
-
-
-@pytest.fixture(scope="session")
-def session_comment_id(client, session_page_id) -> str:
-    parent = {"page_id": session_page_id}
-    rich_text = [
-        {
-            "text": {
-                "content": "This is a test comment.",
-            },
-        },
-    ]
-
-    response = client.comments.create(parent=parent, rich_text=rich_text)
-    return response["id"]
-
-
-def compare_cleaned_ids(id1: str, id2: str) -> bool:
-    return id1.replace("-", "").strip() == id2.replace("-", "").strip()
-
-
-def test_pages_retrieve(client, session_page_id, page_id):
-    response = client.pages.retrieve(page_id=session_page_id)
-    assert response["object"] == "page"
-    assert compare_cleaned_ids(response["parent"]["page_id"], page_id)
-
-
-def test_pages_update(client, session_page_id):
-    icon = {"type": "emoji", "emoji": "🛴"}
-
-    response = client.pages.update(page_id=session_page_id, icon=icon)
-    assert response["icon"] == icon
-
-
-def test_pages_properties_retrieve(client, session_page_id):
-    response = client.pages.properties.retrieve(
-        page_id=session_page_id, property_id="title"
-    )
-    assert response["results"][0]["type"] == "title"
-
-
+@pytest.mark.vcr()
 def test_blocks_children_list(client, session_page_id, session_block_id):
     response = client.blocks.children.list(block_id=session_page_id)
     assert response["object"] == "list"
@@ -93,6 +90,7 @@ def test_blocks_children_list(client, session_page_id, session_block_id):
     assert compare_cleaned_ids(response["results"][0]["id"], session_block_id)
 
 
+@pytest.mark.vcr()
 def test_blocks_retrieve(client, session_block_id):
     response = client.blocks.retrieve(block_id=session_block_id)
     assert response["object"] == "block"
@@ -100,6 +98,7 @@ def test_blocks_retrieve(client, session_block_id):
     assert compare_cleaned_ids(response["id"], session_block_id)
 
 
+@pytest.mark.vcr()
 def test_blocks_update(client, session_block_id):
     new_plain_text = "I'm an updated paragraph."
     new_text = {
@@ -112,42 +111,44 @@ def test_blocks_update(client, session_block_id):
     }
     response = client.blocks.update(block_id=session_block_id, paragraph=new_text)
 
-    assert compare_cleaned_ids(response["id"], session_block_id)
     assert response["paragraph"]["rich_text"][0]["plain_text"] == new_plain_text
 
 
+@pytest.mark.vcr()
 def test_blocks_delete(client, session_block_id):
-    response = client.blocks.delete(block_id=session_block_id)
-
-    assert compare_cleaned_ids(response["id"], session_block_id)
+    client.blocks.delete(block_id=session_block_id)
 
     new_retrieve = client.blocks.retrieve(block_id=session_block_id)
     assert new_retrieve["archived"]
 
 
+@pytest.mark.vcr()
 def test_users_list(client):
     response = client.users.list()
     assert response["type"] == "user"
     assert response["results"][0]["name"] == "Notion Testing Account"
 
 
+@pytest.mark.vcr()
 def test_users_me(client):
     response = client.users.me()
     assert response["type"] == "bot"
     assert response["id"]
 
 
+@pytest.mark.vcr()
 def test_users_retrieve(client):
     me = client.users.me()
     response = client.users.retrieve(me["id"])
     assert response == me
 
 
+@pytest.mark.vcr()
 def test_search(client, page_id):
     payload = {
         "query": "Test",
         "sort": {
-            "direction": "ascending",
+            "direction": "descending",
             "timestamp": "last_edited_time",
         },
     }
@@ -157,6 +158,23 @@ def test_search(client, page_id):
     assert compare_cleaned_ids(response["results"][0]["id"], page_id)
 
 
+@pytest.mark.vcr()
+def test_databases_create(client, session_page_id):
+    db_name = f"Test Database - {datetime.now()}"
+    properties = {
+        "Name": {"title": {}},  # required property
+    }
+    title = [{"type": "text", "text": {"content": db_name}}]
+    parent = {"type": "page_id", "page_id": session_page_id}
+    response = client.databases.create(
+        **{"parent": parent, "title": title, "properties": properties}
+    )
+
+    assert response["object"] == "database"
+    assert compare_cleaned_ids(response["parent"]["page_id"], session_page_id)
+
+
+@pytest.mark.vcr()
 def test_databases_query(client, session_database_id):
     query = {
         "database_id": session_database_id,
@@ -168,14 +186,14 @@ def test_databases_query(client, session_database_id):
     assert not response["results"]
 
 
+@pytest.mark.vcr()
 def test_databases_retrieve(client, session_database_id, session_page_id):
 
     response = client.databases.retrieve(session_database_id)
-    assert compare_cleaned_ids(response["id"], session_database_id)
     assert response["object"] == "database"
-    assert compare_cleaned_ids(response["parent"]["page_id"], session_page_id)
 
 
+@pytest.mark.vcr()
 def test_databases_update(client, session_database_id):
     icon = {"type": "emoji", "emoji": "🔥"}
 
@@ -183,11 +201,32 @@ def test_databases_update(client, session_database_id):
     assert response["icon"] == icon
 
 
-def test_comments_list(client, session_page_id, session_comment_id):
+@pytest.mark.vcr()
+def test_comments_create(client, session_page_id):
+    parent = {"page_id": session_page_id}
+    rich_text = [
+        {
+            "text": {
+                "content": "This is a test comment.",
+            },
+        },
+    ]
+
+    response = client.comments.create(parent=parent, rich_text=rich_text)
+    assert response
+
+
+@pytest.mark.vcr()
+def test_comments_list(client, session_page_id):
 
     response = client.comments.list(block_id=session_page_id)
 
     assert response["object"] == "list"
     assert response["results"]
     assert response["results"][0]["object"] == "comment"
-    assert response["results"][0]["id"] == session_comment_id
+
+
+@pytest.mark.vcr()
+def test_pages_delete(client, session_page_id) -> str:
+    response = client.blocks.delete(block_id=session_page_id)
+    assert response
