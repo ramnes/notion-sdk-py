d5e914ada839e9371ecbc4c613a018ce48778dff
ramnes <contact@ramnes.eu>
2022-12-10 15:05:49 +0100

    Release 2.0.0 🎉

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def get_description():
 
 setup(
     name="notion-client",
-    version="1.0.0",
+    version="2.0.0",
     url="https://github.com/ramnes/notion-sdk-py",
     author="Guillaume Gelin",
     author_email="contact@ramnes.eu",
