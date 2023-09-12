46a481ac3bb9204f7ed0dff4427954df795e9080
guacs <guacs.guacs@gmail.com>
2023-09-12 20:04:38 +0530

    feat: add types for users responses

---
+++ notion_client/types.py
@@ -0,0 +1,55 @@
+"""The types for the various responses."""
+
+from typing import Any, Literal, TypedDict
+
+
+class EmptyObject(TypedDict):
+    ...
+
+
+# ---------- User ----------
+
+
+class PersonDetails(TypedDict):
+    email: str | None
+
+
+class PersonUserObjectResponse(TypedDict):
+    type: Literal["person"]
+    person: PersonDetails
+    name: str | None
+    avatar_url: str | None
+    id: str
+    object: Literal["user"]
+
+
+class WorkspaceOwner(TypedDict):
+    type: Literal["workspace"]
+    workspace: Literal[True]
+
+
+class BotDetails(TypedDict):
+    owner: PersonUserObjectResponse | WorkspaceOwner
+    workspace_name: str | None
+
+
+class BotUserObjectResponse(TypedDict):
+    type: Literal["bot"]
+    bot: EmptyObject | BotDetails
+    name: str | None
+    avatar_url: str | None
+    id: str
+    object: Literal["user"]
+
+
+UserObjectResponse = PersonUserObjectResponse | BotUserObjectResponse
+
+
+# ---------- Block ----------
+BlockObjectResponse = Any
+
+# ---------- Database ---------
+DatabaseObjectResponse = Any
+
+# ---------- Page ----------
+PageObjectResponse = Any
