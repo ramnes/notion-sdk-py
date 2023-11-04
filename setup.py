6e00bacb446dfc62351e5c63425052dbfb8e53fb
ramnes <contact@ramnes.eu>
2023-11-04 15:32:09 +0100

    Add support for Python 3.12

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
     ],
