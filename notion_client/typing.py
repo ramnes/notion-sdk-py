74b3a606a4c2b84ad699c46defb9494ac8f0dd0b
Satoshi <6world4trigger@gmail.com>
2024-10-27 07:33:06 +0800

    change bound of `ResponseType` to `Mapping` from `Dict` (`TypedDict` is not subclass of `Dict`)

--- notion_client/typing.py
+++ notion_client/typing.py
@@ -1,5 +1,5 @@
 """Custom type definitions for notion-sdk-py."""
-from typing import TYPE_CHECKING, Awaitable, TypeVar, Union
+from typing import TYPE_CHECKING, Any, Awaitable, Mapping, TypeVar, Union
 
 if TYPE_CHECKING:  # pragma: no cover
     from notion_client.client import BaseClient
@@ -8,4 +8,4 @@ T = TypeVar("T")
 SyncAsync = Union[T, Awaitable[T]]
 
 ClientType = TypeVar("ClientType", bound=BaseClient)
-ResponseType = TypeVar("ResponseType", bound=dict)
+ResponseType = TypeVar("ResponseType", bound=Mapping[Any, Any])
