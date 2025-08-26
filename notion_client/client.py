7eba4e60edbbd71ff3853f3b8ce8d408c0ed2a2c
ramnes <contact@ramnes.eu>
2025-08-26 12:33:34 +0200

    Release 2.5.0 ✨

--- notion_client/client.py
+++ notion_client/client.py
@@ -92,7 +92,7 @@ class BaseClient:
         client.headers = httpx.Headers(
             {
                 "Notion-Version": self.options.notion_version,
-                "User-Agent": "ramnes/notion-sdk-py@2.4.0",
+                "User-Agent": "ramnes/notion-sdk-py@2.5.0",
             }
         )
         if self.options.auth:
